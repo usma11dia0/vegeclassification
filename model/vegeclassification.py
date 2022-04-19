8b67458c17f4c86b751a389aa648cb16d6656073
usma11dia0 <usma11dia0@gmail.com>
2022-04-20 02:32:26 +0900

    init

---
+++ model/vegeclassification.py
@@ -0,0 +1,46 @@
+#ファインチューニング(全結合層を調整)
+class Net(pl.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+
+        self.feature = resnet18(pretrained=True)
+        self.fc = nn.Linear(1000, 3)
+
+
+    def forward(self, x):
+        h = self.feature(x)
+        h = self.fc(h)
+        return h
+
+
+    def training_step(self, batch, batch_idx):
+        x, t = batch
+        y = self(x)
+        loss = F.cross_entropy(y, t)
+        self.log('train_loss', loss, on_step=False, on_epoch=True)
+        self.log('train_acc', accuracy(y.softmax(dim=-1), t), on_step=False, on_epoch=True)
+        return loss
+
+
+    def validation_step(self, batch, batch_idx):
+        x, t = batch
+        y = self(x)
+        loss = F.cross_entropy(y, t)
+        self.log('val_loss', loss, on_step=False, on_epoch=True)
+        self.log('val_acc', accuracy(y.softmax(dim=-1), t), on_step=False, on_epoch=True)
+        return loss
+
+
+    def test_step(self, batch, batch_idx):
+        x, t = batch
+        y = self(x)
+        loss = F.cross_entropy(y, t)
+        self.log('test_loss', loss, on_step=False, on_epoch=True)
+        self.log('test_acc', accuracy(y.softmax(dim=-1), t), on_step=False, on_epoch=True)
+        return loss
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
+        return optimizer
