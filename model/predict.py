17d900cb3dceb7eb28361b3b3db8ee63bf8cfda1
usma11dia0 <usma11dia0@gmail.com>
2022-04-21 04:15:32 +0900

    add classify model

--- model/predict.py
+++ model/predict.py
@@ -11,6 +11,7 @@ from torchvision.models import resnet18
 import pytorch_lightning as pl
 import torchmetrics
 from torchmetrics.functional import accuracy
+from PIL import Image
 
 
 #ファインチューニング(全結合層を調整)
@@ -59,3 +60,39 @@ class Net(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
         return optimizer
+
+
+#推論 前処理用
+def classify(img_url):
+
+    #推論前の前処理
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor()
+    ])
+
+    target_img = transform(Image.open(img_url))
+    target_img = target_img.unsqueeze(0)
+
+    #推論の実行
+    #ネットワークの準備（推論モード)
+    net = Net().cpu().eval()
+    #重みの読込
+    net.load_state_dict(torch.load("model/vegetable.pt",map_location=torch.device('cpu')))
+
+    #予測値の計算
+    result = net(target_img).float()
+
+    #確率に変換
+    result_proba = F.softmax(result, dim=1)
+
+    #確率.ラベル取得
+    y_proba = torch.max(result_proba).item()
+    y = torch.argmax(result_proba).item()
+
+    #結果表記調整
+    y_proba = y_proba*100
+
+    #推論結果をHTMLに渡す
+    return y, y_proba
--- send_image_app/views.py
+++ send_image_app/views.py
@@ -1,22 +1,9 @@
+from unittest import result
 from django.shortcuts import render
 
-from model.predict import Net
+from model.predict import classify
 from .forms import ImageForm
 from .models import ModelFile
-
-#モデル学習用のimport ※pip install要
-import joblib
-import numpy as np
-import pandas as pd
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-from torchvision import transforms, datasets
-from torchvision.models import resnet18
-import pytorch_lightning as pl
-import torchmetrics
-from torchmetrics.functional import accuracy
 
 def image_upload(request):
     if request.method == 'POST':
@@ -25,33 +12,18 @@ def image_upload(request):
             form.save()
             img_name = request.FILES['image']
             img_url = 'media/documents/{}'.format(img_name)
-        return render(request, 'send_image_app/classify.html', {'img_url':img_url})
+            y, y_proba = classify(img_url)
+
+            #推論結果を保存
+            modelfile = ModelFile.objects.order_by('id').reverse()[0]
+            modelfile.proba = y_proba
+            modelfile.result = y
+            modelfile.save()
+
+        return render(request,
+                     'send_image_app/classify.html',
+                     {'y':y, 'y_proba':round(y_proba,2),'img_url':img_url}
+                     )
     else:
         form = ImageForm()
         return render(request, 'send_image_app/index.html', {'form':form})
-
-
-def classify(request):
-#学習済みモデルの読込
-#loaded_model = joblib.load('model/predict.py')
-#loaded_param = joblib.load('model/vegetable.pt')
-
-    #Uploadされたデータを取得
-    #data = ModelFile.objects.order_by('id').reverse().values_list('images') 
-    #dataset = datasets.ImageFolder('/content/drive/MyDrive/Colab Notebooks/長期AI人材コース/機械学習アプリ/vegetable_images/train', transform)
-
-    #前処理とアノテーション
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
-
-    #推論の実行
-    #学習済み重みをモデルへ付与する
-    # classify_model = Net(data)
-    # y = classify_model.predict(data)
-    # y_proba = classify_model.predict_proba(data)
-
-    #推論結果をHTMLに渡す
-    # return render(request,'send_image_app/classify.html',{'y':y, 'y_proba':y_proba})
